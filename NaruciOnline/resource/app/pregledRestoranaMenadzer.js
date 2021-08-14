377c63c251b4697c3c63daa5924db022b50ccd80
SrdjanBeric <srdjan.beric123@gmail.com>
2021-08-14 17:16:53 +0200

    prikaz restorana

--- NaruciOnline/resource/app/pregledRestoranaMenadzer.js
+++ NaruciOnline/resource/app/pregledRestoranaMenadzer.js
@@ -2,16 +2,26 @@ Vue.component("pregledRestoranaMenadzer",{
     data: function(){
         return{
           korisnik:null,
-          restoran:null
+          restoran:null,
+          statusRestorana:null
         }
     },
 
     template:`
         <div>
-            <p class="card-text">
-                Naziv restorana: {{restoran.naziv}} <br/>
-                Tip: {{restoran.tip}} <br/>
-                Status: {{restoran.status}} <br/>
+            <h2>{{restoran.naziv}}</h2>
+            <p>
+                <b>Tip:</b> {{restoran.tip}} <br>
+                <b>Status:</b> {{ restoran.status ? 'Otvoreno' : 'Zatvoreno' }} <br>
+                <hr>
+                <b>Geografska sirina:</b> {{restoran.lokacija.geoSirina}} <br>
+                <b>Geografska duzina:</b> {{restoran.lokacija.geoDuzina}} <br>
+                <b>Grad:</b> {{restoran.lokacija.grad}} <br>
+                <b>Ulica:</b> {{restoran.lokacija.ulica}} <br>
+                <b>Broj:</b> {{restoran.lokacija.broj}} <br>
+                <b>Postanski broj:</b> {{restoran.lokacija.postanskiBroj}} <br>
+                <hr>
+                <b>Menadzer:</b> {{restoran.menadzer.ime}} {{restoran.menadzer.prezime}} <br>
             </p>
         </div>
     `,
@@ -25,8 +35,6 @@ Vue.component("pregledRestoranaMenadzer",{
             console.log(response.data)
             this.restoran=response.data;})
         
-    },
-    method(){
-        restoran=this.restoran
-    }
+        },
+
 })
