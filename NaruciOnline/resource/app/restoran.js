1b6e8141f38eb8a19ea0a99dd3525c36ad8a64f3
MitarBrankovic <brankovic.ra198.2018@uns.ac.rs>
2021-09-06 21:02:41 +0200

    if=restoran greska

--- NaruciOnline/resource/app/restoran.js
+++ NaruciOnline/resource/app/restoran.js
@@ -16,26 +16,37 @@ Vue.component("restoran",{
             izmeniClick:false,
             izmeniArtikalClick:false,
             artikalZaIzmenu:null,
-            Korpa:null
+            Korpa:null,
+
+            geografskaDuzina:"",
+			geografskaSirina:"",
+			grad:"",
+			ulica:"",
+			broj:"",
+			postanskiBroj:"",
         }
     },
 
     template:`
+        <div >
+        <div id="js-map1" style="height:500px; width:50%;"></div><br><br>
         <div v-if="restoran" >
+
             <div class="container-xl" id="moj-restoran">
                 <h2>{{restoran.naziv}}</h2>
+                
                 <img :src="restoran.slika" width = "200px" heigth = "300">
                 <br><br>
                 <button class="btn btn-primary" type="button" v-on:click="otvoriRecenzije()">Recenzije</button><br><br>
                 <div v-if="!izmeniClick">
-                    <button v-if="uloga==='ADMIN'" type="button" v-on:click="izmeniInformacije()">Izmeni</button>
+                    <button class="btn btn-primary" v-if="uloga==='ADMIN'" type="button" v-on:click="izmeniInformacije()">Izmeni</button>
                 </div>
                 <div v-else>
-                    <button type="button" v-on:click="sacuvajIzmene()">Sacuvaj</button>
-                    <button type="button" v-on:click="izmeniInformacije()">Otkazi</button>
+                    <button class="btn btn-success" type="button" v-on:click="sacuvajIzmene()">Sacuvaj</button>
+                    <button class="btn btn-danger" type="button" v-on:click="izmeniInformacije()">Otkazi</button>
                 </div>
                 <div v-if="!izmeniClick">
-                    <p>
+                    <p><br>
                         <b>Tip:</b> {{restoran.tip}} <br>
                         <b>Status:</b> {{ restoran.status ? 'Otvoreno' : 'Zatvoreno' }} <br>
                         <hr>
@@ -51,7 +62,7 @@ Vue.component("restoran",{
                     </p>
                 </div>
                 <div v-else>
-                    <p>
+                    <p><br>
                         <b>Tip:</b> <input type="text" v-model="restoran.tip"> <br>
                         <b>Status:</b> {{ restoran.status ? 'Otvoreno' : 'Zatvoreno' }} <br>
                         <!--<select v-model="restoran.status">
@@ -59,14 +70,20 @@ Vue.component("restoran",{
                             <option :value="false">Zatvoreno</option>
                         </select> <br>-->
                         <hr>
-                        <b>Geografska sirina:</b> <input type="text" v-model="restoran.lokacija.geoSirina"> <br>
-                        <b>Geografska duzina:</b> <input type="text" v-model="restoran.lokacija.geoDuzina"> <br>
-                        <b>Grad:</b> <input type="text" v-model="restoran.lokacija.grad"> <br>
-                        <b>Ulica:</b> <input type="text" v-model="restoran.lokacija.ulica"> <br>
-                        <b>Broj:</b> <input type="text" v-model="restoran.lokacija.broj"> <br>
-                        <b>Postanski broj:</b> <input type="number" v-model="restoran.lokacija.postanskiBroj"> <br>
-                        <label for="slika"><b>Slika</b></label>
-                        <input type="file"  required @change=imageAdded>
+                        <div class="artikli">
+                            <div class="container">
+                                <label class="col-sm-2 col-form-label"><b>Geografska sirina:</b></label> <input class="col-sm-2 col-form-control" id="geoSir" type="text" v-model="restoran.lokacija.geoSirina"> <br>
+                                <label class="col-sm-2 col-form-label"><b>Geografska duzina:</b></label> <input class="col-sm-2 col-form-control" id="geoDuz" type="text" v-model="restoran.lokacija.geoDuzina"> <br>
+                                <label class="col-sm-2 col-form-label"><b>Grad:</b></label> <input id="grad" class="col-sm-2 col-form-control" type="text" v-model="restoran.lokacija.grad"> <br>
+                                <label class="col-sm-2 col-form-label"><b>Ulica:</b></label> <input id="ulica" class="col-sm-2 col-form-control" type="text" v-model="restoran.lokacija.ulica"> <br>
+                                <label class="col-sm-2 col-form-label"><b>Broj:</b></label> <input id="broj" class="col-sm-2 col-form-control" type="text" v-model="restoran.lokacija.broj"> <br>
+                                <label class="col-sm-2 col-form-label"><b>Postanski broj:</b></label> <input class="col-sm-2 col-form-control" id="postanskiBroj" type="number" v-model="restoran.lokacija.postanskiBroj"> <br>
+                                <label class="col-sm-2 col-form-label" for="slika"><b>Slika</b></label>
+                                <input type="file"  required @change=imageAdded>
+                            </div>
+                            
+                        
+                        </div>
                         <hr>
                         <b>Menadzer:</b> {{restoran.menadzer.ime}} {{restoran.menadzer.prezime}} <br>
                         <hr>    
@@ -171,8 +188,10 @@ Vue.component("restoran",{
                 </div>
             </div>
         </div> 
+        </div>
     `,
     mounted(){
+        init1()
         if(JSON.parse(localStorage.getItem('Korpa')) !== null){
             this.Korpa = JSON.parse(localStorage.getItem('Korpa'))
         }
@@ -191,6 +210,7 @@ Vue.component("restoran",{
             this.artikli = this.restoran.artikli
             console.log(this.artikli)
         })
+        
     },
     methods:{
         dodajArtikalClick:function(){
@@ -379,7 +399,82 @@ Vue.component("restoran",{
                 }
             }
             return count;
-        }
+        },
+        sacuvajLokaciju(){
+			this.geografskaDuzina = document.getElementById("geoDuz").value;
+			this.geografskaSirina = document.getElementById("geoSir").value;
+			this.grad = document.getElementById("grad").value;
+			this.ulica = document.getElementById("ulica").value;
+			this.broj = document.getElementById("broj").value;
+			this.postanskiBroj = document.getElementById("postanskiBroj").value;
+
+			document.getElementById("geoDuz").value = this.geografskaDuzina;
+			document.getElementById("geoSir").value = this.geografskaSirina;
+			document.getElementById("grad").value = this.grad;
+			document.getElementById("ulica").value = this.ulica;
+			document.getElementById("broj").value = this.broj;
+			document.getElementById("postanskiBroj").value = this.postanskiBroj;
+		}
 
     }
-})
+});
+
+function init1(){
+	const map1 = new ol.Map({
+		view: new ol.View({
+			center: [2208254.0327390943,5661276.834908611],
+			zoom: 15
+		}),
+		layers: [
+			new ol.layer.Tile({
+				source: new ol.source.OSM()
+			})
+		],
+		target: 'js-map1'
+	})
+	var previousLayer1 = null;
+	map1.on('click', function(e){
+		if(previousLayer1!=null) {map1.removeLayer(previousLayer1)}
+		var latLong1 = ol.proj.transform(e.coordinate, 'EPSG:3857', 'EPSG:4326');
+		console.log(latLong1);
+		this.geografskaDuzina = latLong1[0]
+		this.geografskaSirina = latLong1[1]
+		
+		var layer1 = new ol.layer.Vector({
+			source: new ol.source.Vector({
+				features: [
+					new ol.Feature({
+						geometry: new ol.geom.Point(ol.proj.fromLonLat(latLong1))
+					})
+				]
+			})
+		});	
+		previousLayer1 = layer1;
+		map1.addLayer(layer1);
+		//simpleReverseGeocoding(this.geografskaDuzina, this.geografskaSirina)
+	})
+}
+
+/*function simpleReverseGeocoding(lon, lat) {
+	fetch('http://nominatim.openstreetmap.org/reverse?format=json&lon=' + lon + '&lat=' + lat).then(function(response) {
+	  	return response.json();
+	}).then(function(json) {
+		ispisAdrese(json, lon, lat);
+	})
+  }
+
+function ispisAdrese(json, lon, lat) {
+	var adresa = json.address;
+	document.getElementById("geoDuz").value = lon;
+	document.getElementById("geoSir").value = lat;
+	var grad = adresa.city
+	if(grad.includes("City")){
+		grad = grad.replace(' City', "")
+	}else if(grad.includes("Municipality")){
+		grad = grad.replace(' Municipality', "")
+	}
+	document.getElementById("grad").value = grad;
+	document.getElementById("ulica").value = adresa.road;
+	document.getElementById("broj").value = adresa.house_number;
+	document.getElementById("postanskiBroj").value = adresa.postcode;
+}*/
